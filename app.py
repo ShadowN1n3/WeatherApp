5445ba78cd0c0d14cca3e9ddfc780131a69872a7
timok <timok999@googlemail.com>
2025-01-18 15:26:39 +0100

    change background, add favicon

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import threading
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, send_from_directory
 from controller.data_fetcher import DataFetcher
 
 app = Flask(__name__)
@@ -15,6 +15,9 @@ def get_data():
 def home():
     return render_template("index.html")
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory('static', 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     threading.Thread(target=data_fetcher.fetch_data_periodically, daemon=True).start()
